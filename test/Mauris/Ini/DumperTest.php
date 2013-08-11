bd8587ed08fcb98b28adbc68cea05c4f99eb4ea4
Sam-Mauris Yong <mauris@hotmail.sg>
2013-08-12 07:35:51 +0800

    updated test coverage

--- test/Mauris/Ini/DumperTest.php
+++ test/Mauris/Ini/DumperTest.php
@@ -6,12 +6,13 @@ class DumperTest extends \PHPUnit_Framework_TestCase
     public function testDump()
     {
         $dumper = new Dumper();
-        $result = $dumper->dump(array('test' => array('value' => true)));
+        $result = $dumper->dump(array('test' => array('value' => true, 'five' => 5, 'str' => 'i"m a boy')));
 
         $expected = <<<EOT
 [test]
 value=true
-
+five=5
+str="i\"m a boy"
 
 EOT;
         $this->assertEquals($expected, $result);
